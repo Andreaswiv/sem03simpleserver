ba21a46a6ed66f2edfa6a028b5237e8d6c0f20e6
Andreaswiv <andreaswiv@uia.no>
2023-04-28 11:48:52 +0000

    ok

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Andreaswiv/sem03simpleserver.git
+
+go 1.18
+
+require (
+	github.com/Andreaswiv/funtemps v0.0.0-20230425104717-9a834c219cd7
+	github.com/Andreaswiv/is105sem03 v0.0.0-20230428113401-79588bd30feb
+)
--- tcp_server.go
+++ tcp_server.go
@@ -4,11 +4,16 @@ import (
 	"io"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 	"sync"
+    "fmt"
+
+	"github.com/Andreaswiv/funtemps/conv"
+	"github.com/Andreaswiv/is105sem03/mycrypt"
 )
 
 func main() {
-
 	var wg sync.WaitGroup
 
 	server, err := net.Listen("tcp", "172.17.0.3:8999")
@@ -16,41 +21,66 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Printf("bundet til %s", server.Addr().String())
+
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
+
 		for {
-			log.Println("før server.Accept() kallet")
 			conn, err := server.Accept()
 			if err != nil {
 				return
 			}
-			go func(c net.Conn) {
-				defer c.Close()
-				for {
-					buf := make([]byte, 1024)
-					n, err := c.Read(buf)
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-					switch msg := string(buf[:n]); msg {
-  				        case "ping":
-						_, err = c.Write([]byte("pong"))
-					default:
-						_, err = c.Write(buf[:n])
-					}
-					if err != nil {
-						if err != io.EOF {
-							log.Println(err)
-						}
-						return // fra for løkke
-					}
-				}
-			}(conn)
+
+			go handleConnection(conn)
 		}
 	}()
+
 	wg.Wait()
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		if err != io.EOF {
+			log.Println(err)
+		}
+		return
+	}
+
+	msg := string(buf[:n])
+	switch {
+	case strings.HasPrefix(msg, "ping"):
+		_, err = conn.Write([]byte("pong"))
+	case strings.HasPrefix(msg, "kjevik"):
+		parts := strings.Split(msg, ";")
+		if len(parts) != 4 {
+			log.Println("invalid message format")
+			return
+		}
+
+		degrees, err := strconv.Atoi(parts[3])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fahrenheit := conv.CelsiusToFahrenheit(float64(degrees))
+		response := fmt.Sprintf("%s;%s;%s;%.1f", parts[0], parts[1], parts[2], fahrenheit)
+		_, err = conn.Write([]byte(response))
+	default:
+		dekryptertMelding := mycrypt.Krypter([]rune(msg), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+		log.Println("Dekryptert melding: ", string(dekryptertMelding))
+		_, err = conn.Write([]byte(string(dekryptertMelding)))
+	}
+
+	if err != nil {
+		if err != io.EOF {
+			log.Println(err)
+		}
+		return
+	}
 }
